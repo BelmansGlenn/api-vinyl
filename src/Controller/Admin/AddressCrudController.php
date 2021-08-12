8ec699f285343ac494596584b5dc28c811cd5bc8
BelmansGlenn <belmansglenn@gmail.com>
2021-08-12 14:40:12 +0200

    easyadmin

--- src/Controller/Admin/AddressCrudController.php
+++ src/Controller/Admin/AddressCrudController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Controller\Admin\AddressCrudController;
+namespace App\Controller\Admin;
 
 use App\Entity\Address;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
