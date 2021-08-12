853d6297415410b8b888f936e6f587626ce4271a
BelmansGlenn <belmansglenn@gmail.com>
2021-08-12 14:50:10 +0200

    easyadmin

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,6 +2,14 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Address;
+use App\Entity\Carrier;
+use App\Entity\Category;
+use App\Entity\Order;
+use App\Entity\Product;
+use App\Entity\Review;
+use App\Entity\Tag;
+use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
@@ -27,6 +35,13 @@ class DashboardController extends AbstractDashboardController
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linktoDashboard('Dashboard', 'fa fa-home');
-        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+        yield MenuItem::linkToCrud('User', 'fas fa-users', User::class);
+        yield MenuItem::linkToCrud('Address', 'fas fa-address-book', Address::class);
+        yield MenuItem::linkToCrud('Review', 'fas fa-search', Review::class);
+        yield MenuItem::linkToCrud('Product', 'fab fa-product-hunt', Product::class);
+        yield MenuItem::linkToCrud('Category', 'fas fa-book-open', Category::class);
+        yield MenuItem::linkToCrud('Tag', 'fas fa-tag', Tag::class);
+        yield MenuItem::linkToCrud('Order', 'fas fa-sort-amount-up', Order::class);
+        yield MenuItem::linkToCrud('Carrier', 'fas fa-truck', Carrier::class);
     }
 }
