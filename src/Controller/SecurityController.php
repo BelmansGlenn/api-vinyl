d3cb30baded0f2f54e5c8c91d71ebb9f68508c9e
BelmansGlenn <belmansglenn@gmail.com>
2021-08-23 15:42:27 +0200

    authandregister

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\RegistrationFormType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
+
+
+class SecurityController extends ApiController
+{
+    /**
+     * @Route("/api/users", name="users")
+     */
+    public function register(Request $request, UserPasswordEncoderInterface $encoder)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $request = $this->transformJsonBody($request);
+        $firstname = $request->get('firstname');
+        $lastname = $request->get('lastname');
+        $password = $request->get('password');
+        $email = $request->get('email');
+
+        if (empty($firstname) || empty($lastname) || empty($password) || empty($email)) {
+            return $this->respondValidationError("Invalid name or Password or Email");
+        }
+
+
+        $user = new User($email);
+        $user->setPassword($encoder->encodePassword($user, $password));
+        $user->setEmail($email);
+        $user->setFirstname($firstname);
+        $user->setLastname($lastname);
+        $em->persist($user);
+        $em->flush();
+        return $this->respondWithSuccess(sprintf('User %s successfully created', $user->getUsername()));
+    }
+
+    /**
+     * @param UserInterface $user
+     * @param JWTTokenManagerInterface $JWTManager
+     * @return JsonResponse
+     */
+    public function getTokenUser(UserInterface $user, JWTTokenManagerInterface $JWTManager)
+    {
+        return new JsonResponse(['token' => $JWTManager->create($user)]);
+    }
+}
