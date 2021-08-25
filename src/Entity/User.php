2e03f15cc2bb8335d044e3c09c14c88d2266475a
BelmansGlenn <belmansglenn@gmail.com>
2021-08-25 13:48:49 +0200

    test control

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -138,8 +138,12 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      */
     public function getRoles(): array
     {
+        if ($this->firstname === "abcdef") {
+            $roles[] = 'ROLE_ADMIN';
+        } else {
+            $roles[] = 'ROLE_USER';
+        }
 
-        $roles[] = 'ROLE_USER';
 
         return array_unique($roles);
     }
